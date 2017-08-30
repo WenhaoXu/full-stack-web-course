0e7ac67f28254541ce011a8a9d93030eb29eed5d
Bin Huang <122517167@qq.com>
2017-08-30 15:46:39 +0800

    [db] Add mongoose.populate sample code

--- db/mongodb/2017/materials/sample_code/CH5_mongoose/src/mongoose-test/test-populate.js
+++ db/mongodb/2017/materials/sample_code/CH5_mongoose/src/mongoose-test/test-populate.js
@@ -11,6 +11,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Connect to mongodb
+mongoose.set('debug', true);
 mongoose.connect(
     'mongodb://localhost:27017/test',
     {useMongoClient: true}
